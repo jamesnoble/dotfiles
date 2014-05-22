77622f9cf955565d146043cc457e37b3969ec607
Jamwillinob <james@jamwillinob.com>
2014-05-22 17:45:15 +0100

    Update vim alias

--- vim/alias.zsh
+++ vim/alias.zsh
@@ -1,4 +1,4 @@
 if [[ $(uname) == "Darwin" ]]
 then
-	alias vim='/usr/local/Cellar/macvim/7.4-70/MacVim.app/Contents/MacOS/Vim'
+	alias vim='/usr/local/Cellar/macvim/7.4-73/MacVim.app/Contents/MacOS/Vim'
 fi
