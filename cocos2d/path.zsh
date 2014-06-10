95476600d01722342daae477f1db7ea1b744b745
Jamwillinob <james@jamwillinob.com>
2014-06-10 20:07:08 +0100

    Add cocos to path

---
+++ cocos2d/path.zsh
@@ -0,0 +1,4 @@
+
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT=/Users/james/code/opensource/cocos2d-js/tools/cocos2d-console/bin
+export PATH=$COCOS_CONSOLE_ROOT:$PATH
