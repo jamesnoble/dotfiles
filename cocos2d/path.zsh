45db4cccd0c3c583fe6e01e1c72cd903483af46f
Jamwillinob <james@jamesnoble.net>
2015-02-19 21:29:35 +0000

    Fix cocos path for cocos2d-x

--- cocos2d/path.zsh
+++ cocos2d/path.zsh
@@ -1,3 +1,7 @@
 # Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
-export COCOS_CONSOLE_ROOT=/Users/james/code/opensource/cocos2d-console/bin
+export COCOS_CONSOLE_ROOT=/Users/james/code/opensource/cocos2d-x/tools/cocos2d-console/bin
 export PATH=$COCOS_CONSOLE_ROOT:$PATH
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+export COCOS_TEMPLATES_ROOT=/Users/james/code/opensource/cocos2d-x/templates
+export PATH=$COCOS_TEMPLATES_ROOT:$PATH
