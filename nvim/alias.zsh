2c35c90b35fa23d7a93bbba649d1c593847f8316
James Noble <james@jamesnoble.net>
2015-06-15 16:25:42 +0100

    Remove old vim alias

--- nvim/alias.zsh
+++ nvim/alias.zsh
@@ -1,5 +1,1 @@
-if [[ $(uname) == "Darwin" ]]
-then
-    alias vim='/usr/local/Cellar/macvim/7.4-73_1/MacVim.app/Contents/MacOS/Vim'
-    alias nvim-update='nvim +PluginInstall +qall'
-fi
+alias nvim-update='nvim +PluginInstall +qall'
