9de00c1c44983201a30205c0b16296a0ac941f57
James Noble <james@jamesnoble.net>
2015-12-03 14:31:55 +0000

    Fix install.sh scripts for node and xcode

--- xcode/install.sh
+++ xcode/install.sh
@@ -1,1 +1,1 @@
-ln -s base16-tomorrow.dark.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+ln -s xcode/base16-tomorrow.dark.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
