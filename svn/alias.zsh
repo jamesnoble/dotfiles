27e5d8e7766a50f1a07153379caaaf615e51707b
James Noble <james@jamesnoble.net>
2016-05-12 17:05:33 +0100

    Turn sl alias into a function so it can take parameters and pass them through to svn log

--- svn/alias.zsh
+++ svn/alias.zsh
@@ -1,6 +1,10 @@
 alias ss='svn status --ignore-externals -u'
 alias sm='svn status --ignore-externals -u | ag --nocolor "M "'
 alias su='svn update'
-alias sl='svn log --limit 10 | svn-short-log'
 alias sc='svn commit'
 alias sd='svn diff --diff-cmd colordiff'
+
+function sl()
+{
+  svn log --limit 10 $@ | svn-short-log
+}
