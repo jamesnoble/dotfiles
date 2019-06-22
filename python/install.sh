87ddc2642a5fd3827cfe311b3e47976eabb31e1e
James Noble <james@jamesnoble.net>
2019-06-22 22:47:39 +0100

    Fix python install.sh on WSL

--- python/install.sh
+++ python/install.sh
@@ -1,2 +1,11 @@
-sudo easy_install pip
+if type apt-get &> /dev/null
+then
+    sudo apt-get --assume-yes install python
+    sudo apt-get --assume-yes install python-setuptools
+fi
+
+if ! type pip &> /dev/null
+then
+    sudo easy_install pip
+fi
 sudo pip install pylint
