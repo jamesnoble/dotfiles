d0a53db2ef26dcbcbfddf8f4945f32130cf73666
James Noble <james@jamesnoble.net>
2021-10-24 19:58:27 +0100

    Add install script for windows terminal settings

---
+++ windows_terminal/install.ps1
@@ -0,0 +1,1 @@
+New-Item -Force -Path $Env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value $Env:userprofile\dotfiles\windows_terminal\settings.json
