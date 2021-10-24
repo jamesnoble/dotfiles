b469bffe3e4851baa5bb2a8caf258b5347b2f202
James Noble <james@jamesnoble.net>
2021-10-24 21:16:14 +0100

    Vi mode for PSReadLine

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -10,7 +10,31 @@ function prompt()
 
 $dotfiles = "$home\dotfiles"
 
+# Sourcing sub-dir files
+
 foreach($item in Get-ChildItem $dotfiles -Recurse -Filter "aliases.ps1")
 {
     . $item
 }
+
+# PSReadLine
+
+$PSReadLineOptions = @{
+    EditMode = "Vi"
+}
+Set-PSReadlineOption @PSReadLineOptions
+
+function OnViModeChange
+{
+    if ($args[0] -eq 'Command')
+    {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    }
+    else
+    {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
